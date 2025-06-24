5df0867edf6ce5d88d364bea2a7f1177c01d65bb
sumeyaahmed98 <sumeyaahmed@gmail.com>
2025-06-24 13:49:01 +0300

    d

--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,7 @@ from countdown import countdown, countdown_with_sleep
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+    # Example usage:
+    countdown(5)
+    countdown_with_sleep(5)
